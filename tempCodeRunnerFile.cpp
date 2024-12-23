6cd647c1fc2f3d876a023df5cc50cdb1b4c2e804
abhigit-saha <abhijitsaha1006@gmail.com>
2024-12-23 16:11:57 +0530

    23/11/24

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+ for (int i = 1; i < ctr; i++)
+    {
+        cout << i << ": " << " ";
+        for (int j = 0; j < g[i].size(); j++)
+        {
+            cout << g[i][j] << " ";
+        }
+        cout << endl;
+    }
