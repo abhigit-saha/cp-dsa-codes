b6d9dc22df3a2c3be8eaef39e236c1630940ba24
abhigit-saha <abhijitsaha1006@gmail.com>
2024-12-26 13:29:32 +0530

    26/12/24

---
+++ creating_teams.cpp
@@ -0,0 +1,190 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+#define endl '\n'
+
+#define debarr(a, n)            \
+    cout << #a << ':';          \
+    for (int i = 0; i < n; i++) \
+        cout << a[i] << ' ';    \
+    cout << endl;
+
+// Operator overloading for pairs
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+
+// Operator overloading for vectors
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    for (const auto &it : v)
+        os << it << ' ';
+    return os << ']';
+}
+
+// Operator overloading for sets
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &s)
+{
+    os << '[';
+    for (const auto &it : s)
+        os << it << ' ';
+    return os << ']';
+}
+
+// Operator overloading for unordered sets
+template <typename T>
+ostream &operator<<(ostream &os, const unordered_set<T> &us)
+{
+    os << '[';
+    for (const auto &it : us)
+        os << it << ' ';
+    return os << ']';
+}
+
+// Operator overloading for maps
+template <typename K, typename V>
+ostream &operator<<(ostream &os, const map<K, V> &m)
+{
+    os << '[';
+    for (const auto &it : m)
+        os << it << ' ';
+    return os << ']';
+}
+
+// Operator overloading for unordered maps
+template <typename K, typename V>
+ostream &operator<<(ostream &os, const unordered_map<K, V> &um)
+{
+    os << '[';
+    for (const auto &it : um)
+        os << it << ' ';
+    return os << ']';
+}
+
+// Operator overloading for multisets
+template <typename T>
+ostream &operator<<(ostream &os, const multiset<T> &ms)
+{
+    os << '[';
+    for (const auto &it : ms)
+        os << it << ' ';
+    return os << ']';
+}
+
+// Debugging function
+template <typename T>
+void dbs(const string &str, T t)
+{
+    cerr << str << ':' << t << endl;
+}
+
+template <typename T, typename... S>
+void dbs(const string &str, T t, S... s)
+{
+    size_t idx = str.find(',');
+    cerr << str.substr(0, idx) << ':' << t << ',';
+    dbs(str.substr(idx + 1), s...);
+}
+
+template <typename T>
+void prc(T a, T b)
+{
+    cerr << '[';
+    for (T i = a; i != b; ++i)
+    {
+        if (i != a)
+            cerr << ',';
+        cerr << *i;
+    }
+    cerr << ']' << endl;
+}
+
+// Binary exponentiation
+int binpow(int b, int p, int mod)
+{
+    int ans = 1;
+    b %= mod;
+    while (p)
+    {
+        if (p & 1)
+            ans = ans * b % mod;
+        b = b * b % mod;
+        p >>= 1;
+    }
+    return ans;
+}
+int mod = 1e9 + 7;
+vector<vector<int>> g;
+vector<int> col;
+bool is_bipartite = true;
+void dfs(int node, int par, int colour)
+{
+    if (!is_bipartite)
+        return;
+    col[node] = colour;
+
+    for (auto v : g[node])
+    {
+        if (v == par)
+            continue;
+        if (col[v] == 0)
+        {
+            dfs(v, node, 3 - colour);
+        }
+        else if (col[v] == colour)
+        {
+            is_bipartite = false;
+            return;
+        }
+    }
+}
+void solve()
+{
+    // Your code here
+    int n, m;
+    cin >> n >> m;
+    g.resize(n + 1);
+    col.assign(n + 1, 0);
+    for (int i = 1; i <= m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        g[a].emplace_back(b);
+        g[b].emplace_back(a);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (col[i] == 0)
+        {
+            dfs(i, 0, 1);
+        }
+    }
+
+    if (!is_bipartite)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // int t; cin >> t; while (t--)
+    solve();
+    return 0;
+}
